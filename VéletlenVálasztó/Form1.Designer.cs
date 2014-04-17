467e753679ccf64bcdc1fd03dbee794b7752f647
Czeglédi Áron <aronczegledi98@gmail.com>
2014-04-17 20:31:00 +0200

    Initial commit

---
+++ VéletlenVálasztó/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace VéletlenVálasztó
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.megnyitásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mentésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hozzáadás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(4, 36);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(4, 76);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(537, 129);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 215);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Elem eltávolítása";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(277, 215);
+            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 36);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Lista kiürítése";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(4, 260);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(479, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Választás";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.MaximumSize = new System.Drawing.Size(537, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 304);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.MaximumSize = new System.Drawing.Size(537, 500);
+            this.flowLayoutPanel1.MinimumSize = new System.Drawing.Size(537, 29);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(537, 29);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.megnyitásToolStripMenuItem,
+            this.mentésToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(543, 31);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // megnyitásToolStripMenuItem
+            // 
+            this.megnyitásToolStripMenuItem.Name = "megnyitásToolStripMenuItem";
+            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(100, 27);
+            this.megnyitásToolStripMenuItem.Text = "Megnyitás";
+            this.megnyitásToolStripMenuItem.Click += new System.EventHandler(this.megnyitásToolStripMenuItem_Click);
+            // 
+            // mentésToolStripMenuItem
+            // 
+            this.mentésToolStripMenuItem.Name = "mentésToolStripMenuItem";
+            this.mentésToolStripMenuItem.Size = new System.Drawing.Size(78, 27);
+            this.mentésToolStripMenuItem.Text = "Mentés";
+            this.mentésToolStripMenuItem.Click += new System.EventHandler(this.mentésToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(489, 262);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 36);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button4;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(543, 336);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(565, 500);
+            this.MinimumSize = new System.Drawing.Size(565, 370);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Véletlen választó program";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem megnyitásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mentésToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VéletlenVálasztó/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace VéletlenVálasztó
+{
+    public partial class Form1 : Form
+    {
+        String újElem;
+        Random véletlen;
+        Int32 elemIndex;
+        Int32 választásokSzáma;
+
+        OpenFileDialog megnyitás;
+        Int32 fájlAzonosító;
+        String fájlNév;
+        String fájlÚtvonal;
+        Int32 fájlokSzáma;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            újElem = textBox1.Text;
+            this.Text = "Véletlen választó program";
+            button4.Text = "Választás";
+            választásokSzáma = 0;
+            label1.Text = "0";
+            label2.Text = "";
+
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Add(újElem);
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Írja be a hozzáadni kívánt elemet a szövegdobozba!", "Véletlen választó program", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1 && listBox1.Items.Count != 0)
+            {
+                this.Text = "Véletlen választó program";
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                button4.Text = "Választás";
+                választásokSzáma = 0;
+                label1.Text = "0";
+                label2.Text = "";
+            }
+            else if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Jelölje meg az eltávolítani kívánt elemet!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Adjon hozzá elemeket a listához!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count != 0)
+            {
+                this.Text = "Véletlen választó program";
+                listBox1.Items.Clear();
+                button4.Enabled = true;
+                button4.Text = "Választás";
+                választásokSzáma = 0;
+                label1.Text = "0";
+                label2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Adjon hozzá elemeket a listához!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 1)
+            {
+                véletlen = new Random();
+                elemIndex = véletlen.Next(listBox1.Items.Count);
+
+                if (választásokSzáma < 999)
+                {
+                    választásokSzáma += 1;
+                    label1.Text = Convert.ToString(választásokSzáma);
+                }
+                else
+                {
+                    választásokSzáma = 999;
+                    label1.Text = "999";
+                    button4.Enabled = false;
+                }
+
+                label2.Visible = true;
+                label2.Text = listBox1.Items[elemIndex].ToString();
+                button4.Text = "Választás újra";
+            }
+            else if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Adjon hozzá elemeket a listához!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (listBox1.Items.Count == 1)
+            {
+                MessageBox.Show("Adjon hozzá több elemet a listához!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void megnyitásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            megnyitás = new OpenFileDialog();
+            megnyitás.Filter = "Szöveges dokumentum|*.txt";
+            megnyitás.Title = "Lista megnyitása";
+
+            try
+            {
+                megnyitás.ShowDialog();
+                StreamReader Import = new StreamReader(megnyitás.FileName.ToString(), Encoding.Default);
+
+                while (Import.Peek() >= 0)
+                {
+                    listBox1.Items.Add(Convert.ToString(Import.ReadLine()));
+                }
+
+                this.Text = "Véletlen választó program - Megnyitott fájl: " + megnyitás.SafeFileName;
+                button4.Text = "Választás";
+                választásokSzáma = 0;
+                label1.Text = "0";
+                label2.Text = "";
+            }
+            catch (Exception kivétel)
+            {
+                MessageBox.Show(kivétel.Message, "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void mentésToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fájlÚtvonal = @Application.StartupPath + "\\listák\\";
+            Directory.CreateDirectory(Path.GetDirectoryName(fájlÚtvonal));
+            fájlokSzáma = Directory.GetFiles(fájlÚtvonal, "*.txt", SearchOption.AllDirectories).Count();
+            fájlAzonosító = fájlokSzáma + 1;
+            fájlNév = "lista" + fájlAzonosító + ".txt";
+
+            if (listBox1.Items.Count >= 1)
+            {
+                    try
+                    {
+                        using (TextWriter szövegÍró = new StreamWriter(fájlÚtvonal + fájlNév))
+                        {
+                            foreach (string újElem in listBox1.Items)
+                            {
+                                szövegÍró.WriteLine(újElem);
+                            }
+
+                            MessageBox.Show("A fájl mentése sikeres volt.", "Véletlen választó program",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            MessageBox.Show("A fájl útvonala: " + Path.GetFullPath(fájlÚtvonal + fájlNév),
+                                "Véletlen választó program",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    catch (Exception kivétel)
+                    {
+                        MessageBox.Show(Convert.ToString(kivétel), "Véletlen választó program",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+            }
+            else
+            {
+                MessageBox.Show("Adjon hozzá elemeket a listához!", "Véletlen választó program",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
+
